00572be1db2758e79c1f70df50965cc59657dfc3
Kenj <kpjaculbia@gmail.com>
2024-08-07 09:47:23 +0800

    Inserted enums for JobCandidate (prototype)

---
+++ Ats.Services/Ats.Api/Controllers/EnumsController.cs
@@ -0,0 +1,47 @@
+﻿using ATS.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ats.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnumsController : ControllerBase
+    {
+        [HttpGet("SourcingTool")]
+        public ActionResult<IEnumerable<string>> GetSourcingTools()
+        {
+            var values = Enum.GetNames(typeof(SourcingTool));
+            return Ok(values);
+        }
+
+        [HttpGet("AssignedHr")]
+        public ActionResult<IEnumerable<string>> GetAssignedHrs()
+        {
+            var values = Enum.GetNames(typeof(AssignedHr));
+            return Ok(values);
+        }
+
+        [HttpGet("SalaryNegotiable")]
+        public ActionResult<IEnumerable<string>> GetSalaryNegotiables()
+        {
+            var values = Enum.GetNames(typeof(SalaryNegotiable));
+            return Ok(values);
+        }
+
+        [HttpGet("NoticeDuration")]
+        public ActionResult<IEnumerable<string>> GetNoticeDurations()
+        {
+            var values = Enum.GetNames(typeof(NoticeDuration));
+            return Ok(values);
+        }
+
+        //EnumsController
+        [HttpGet("ApplicationStatus")]
+        public ActionResult<IEnumerable<string>> GetApplicationStatuses()
+        {
+            var values = Enum.GetNames(typeof(ApplicationStatus));
+            return Ok(values);
+        }
+    }
+
+}
---
+++ Ats.Services/Ats.Models/Enums/JobCandidateEnums.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Models.Enums
+{
+    public enum SourcingTool
+    {
+        JobStreet,
+        Referral,
+        LinkedIn,
+        Indeed,
+        Facebook,
+        Jora,
+        Email,
+        OJT,
+        Others
+    }
+
+    public enum AssignedHr
+    {
+        Others,
+        Jam,
+        Khel,
+        Sherlyn
+    }
+
+    public enum SalaryNegotiable
+    {
+        Yes,
+        No
+    }
+
+    public enum NoticeDuration
+    {
+        OneWeekNotice,
+        TwoWeeksNotice,
+        ThreeWeeksNotice,
+        FourWeeksOrMoreNotice
+    }
+
+    public enum ApplicationStatus
+    {
+        InitialInterview,
+        TechnicalInterview,
+        ClientInterview,
+        NoShow,
+        NotShortlisted,
+        RetractedApplication,
+        WaitingForSuitableClient,
+        Blacklisted,
+        SalesAdvice,
+        CongratulatoryEmail,
+        JobOffer,
+        ContractPreparation,
+        Onboarded
+    }
+}
+
